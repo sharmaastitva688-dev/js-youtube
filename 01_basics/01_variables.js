6f9d448b288c18bcbe37f9e0bffa5e9b3bf0febe
sharmaastitva688-dev <sharmaastitva688@gmail.com>
2026-07-05 11:23:54 +0530

    changes2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const name = Astitva
+const RollNo = 2402150100012
+let city = Delhi
+var town = YamunaVihar
+let pincode = 110053
+console.log(name)
+console.table([name, RollNo, city, town, pincode])
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,3 @@
+
 console.log("Astitva")
 console.log("Sharma")
