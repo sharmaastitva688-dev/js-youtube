f376c19e25f7401364f481e4073e0b4d6c9f5e4a
sharmaastitva688-dev <sharmaastitva688@gmail.com>
2026-07-11 07:26:52 +0000

    Conversion of data

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,9 @@
+let score = "33abs"
+console.log(score)
+let afterconversion = Number(score)
+console.log(typeof afterconversion)
+console.log(afterconversion)
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
